cd118118531cfeda78edfa89d653251ff26ea6f0
realkodesh <liraguilherme186@gmail.com>
2024-05-21 19:59:33 -0300

    API Alimentos Project + SwaggerConfig

---
+++ alimentos/src/main/java/br/com/alimento/config/OpenAPIConfig.java
@@ -0,0 +1,26 @@
+package br.com.alimento.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+
+@Configuration
+public class OpenAPIConfig {
+	
+	@Bean
+   public OpenAPI customOpenAPI() {
+	   return new OpenAPI()
+			   .info(new Info()
+			   .title("Alimentos API com Java e Spring Boot")
+			   .version("v1")
+			   .description("Alimentos Web API")
+			   .termsOfService("https://youtube.com")
+			   .license(new License().name("Apache 2.0")
+					   .url("https://youtube.com")));
+   }
+
+}
